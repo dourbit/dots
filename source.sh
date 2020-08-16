27bb61b916ffc7a38e86b7576215ffc7fe0feb66
Orlin M Bozhinov <om@soundsapiens.com>
2020-08-16 16:22:18 +0300

    proper source.sh can only be sourced

--- source.sh
+++ source.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
-
 # NOTE: please edit your own ./custom.sh
 # unless you'd like to do a pull-request upstream to dourbit/dots
 
 if [ -z "$DOTS_HOME" ]; then
   echo "\$DOTS_HOME not set!"
+  return 1
 elif ! [ -d "$DOTS_HOME" ]; then
   echo "\$DOTS_HOME dir of $DOTS_HOME is Not Found!"
+  return 1
 fi
 
 

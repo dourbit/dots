656c8c7bec0bfd65f85a6bd4ca735c4f685f176b
Orlin M Bozhinov <om@soundsapiens.com>
2020-08-19 21:55:17 +0300

    further refactor by upgraded holy-env

--- source.sh
+++ source.sh
@@ -1,9 +1,11 @@
 # NOTE: please edit your own ./custom.sh
 # unless you'd like to do a pull-request upstream to dourbit/dots
 
-# holy-one should be on, or holy-you won't be found
+# holy-one should be on, or holy-env won't be found
 # skip the extra check because that's very unlikely
-holy-you 1 || return 1
+# holy-you will be tested through holy-env and also
+# the 1 asks it to be verbose about holy-you status
+holy-env "$HOLY_LEAD" 1 || return 1
 
 
 # NOTE: this should be kept last

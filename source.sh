42b803e30d7de79d1da440e09230112ac2850462
Orlin M Bozhinov <om@soundsapiens.com>
2020-08-19 21:02:13 +0300

    a holy-you function export to validate DOTS_HOME

--- source.sh
+++ source.sh
@@ -1,13 +1,9 @@
 # NOTE: please edit your own ./custom.sh
 # unless you'd like to do a pull-request upstream to dourbit/dots
 
-if [ -z "$DOTS_HOME" ]; then
-  echo "\$DOTS_HOME not set!"
-  return 1
-elif ! [ -d "$DOTS_HOME" ]; then
-  echo "\$DOTS_HOME dir of $DOTS_HOME is Not Found!"
-  return 1
-fi
+# holy-one should be on, or holy-you won't be found
+# skip the extra check because that's very unlikely
+holy-you 1 || return 1
 
 
 # NOTE: this should be kept last

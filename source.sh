8e4c58e57ca4023b970e7e46eb0ce5bf208ffa77
Orlin M Bozhinov <om@soundsapiens.com>
2020-08-25 16:19:29 +0300

    exports THIS_HOME for the duration of source.sh

--- source.sh
+++ source.sh
@@ -1,14 +1,32 @@
-# NOTE: please edit your own ./custom.sh
+# NOTE: please edit your own ./custom.sh (sourced if created)
 # unless you'd like to do a pull-request upstream to dourbit/dots
+
+#
+export THIS_HOME="$DOTS_HOME"
+# see it unset at the end of this source.sh
+# exists for more concise calls of uses and holy-dot
+# thus this-that needs no path arg, for the time being...
+# only ever an issue when sourcing from the outside, e.g. ~/.bashrc, etc.
+# essential for code portability between blesstheos and dots or its forks -
+# it is an indicator of bootstrapping in progress...
 
 # holy-one should be on, or holy-env won't be found
 # skip the extra check because that's very unlikely
+#
 # holy-you will be tested through holy-env and also
 # the 1 asks it to be verbose about holy-you status
-holy-env 1 || return 1
+# it exports $LEAD_HOME and $NEXT_HOME upon success
+holy-env 1 || {
+  # clean-up here
+  unset THIS_HOME
+  return 1
+}
 
 
 # NOTE: this should be kept last
-if [ -f ${DOTS_HOME}/custom.sh ]; then
+if [ -s ${DOTS_HOME}/custom.sh ]; then
   . ${DOTS_HOME}/custom.sh
 fi
+
+# bootstrapping is done
+unset THIS_HOME
